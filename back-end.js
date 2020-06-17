9a58183f6345c2c5f72bf7e0a74cbd6cbe853d04
play.js <git@gituser.com>
2020-06-16 22:09:35 -0400

    update front(main.js) and back-end.js

---
+++ back-end.js
@@ -0,0 +1,4 @@
+function legally(age){
+  if(age<18){return "You can't do anything";}else if(age >= 18 && age <= 21){return "You can drive but don't drink.";} else {return "You can do either but don't do both at the same time."}
+}
+module.exports = whatCanIDoLegally;
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-
-
+const whatCanIDoLegally = require(',/back-end.js)
+const age = getInput(0);
+const message = whatCanIDoLegally(age);
+console.log(message);
 
 
 function getInput(i) {
