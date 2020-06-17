90e90ab37f6dd1d375cdb25a8a257825c3db71aa
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-06-17 07:39:02 -0400

    revise syntax

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const whatCanIDoLegally = require(',/back-end.js)
+const whatCanIDoLegally = require('./back-end.js');
 const age = getInput(0);
 const message = whatCanIDoLegally(age);
 console.log(message);
